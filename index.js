86e0577ace16739632fcf0b79276b7a0421cbdde
nicozapata-ort <nicolashzap@gmail.com>
2021-03-30 18:20:19 -0300

    Probando StackBlitz

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 //import './clousure_functions.js'
 
 // Write Javascript code!
-const appDiv = document.getElementById('app');
+const appDiv = document.getElementById("app");
 appDiv.innerHTML = `<h1>ES6</h1>`;
 
+//Declaracion de prueba
+console.log("Probando Stackblitz con GitHub");
